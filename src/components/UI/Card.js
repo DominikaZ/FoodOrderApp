892838b1d6da7ee6924c566c27767e164b20b99c
xzemanov <zemanovicova.dominika@gmail.com>
2022-08-26 16:35:03 +0200

    UI card implemented

---
+++ src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import classes from './Card.module.css';
+
+const Card = (props) => {
+  const customClasses = classes.card + ' ' + props.className;
+
+  return <div className={customClasses}>{props.children}</div>;
+};
+
+export default Card;
