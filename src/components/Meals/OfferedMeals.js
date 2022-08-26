e3ccbeac2262a5f5babfc5b1b63d96ad2f6baacd
xzemanov <zemanovicova.dominika@gmail.com>
2022-08-26 22:04:00 +0200

    Cart can see all items

--- src/components/Meals/OfferedMeals.js
+++ src/components/Meals/OfferedMeals.js
@@ -33,7 +33,8 @@ const OfferedMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
-      title={meal.name}
+      id={meal.id}
+      name={meal.name}
       description={meal.description}
       price={meal.price}
     ></MealItem>
