8446c7d6706e235763b2fce0fc332427f5f47507
xzemanov <zemanovicova.dominika@gmail.com>
2022-08-26 18:38:43 +0200

    MealItemForm unique id

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -11,7 +11,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div><MealItemForm></MealItemForm></div>
+      <div><MealItemForm id={props.key}></MealItemForm></div>
     </li>
   );
 };
