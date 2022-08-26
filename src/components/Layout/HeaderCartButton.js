b5661d615d10aed204d1ef814aaed0e9bb3d4872
xzemanov <zemanovicova.dominika@gmail.com>
2022-08-26 23:47:52 +0200

    cart bump added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsBumped, setBtnIsBumped] = useState(false);
   const cartCtx = useContext(CartContext);
-  const numOfCartItems = cartCtx.items.reduce((curNumberAcc, item) => {
+  const {items} = cartCtx;
+
+  const numOfCartItems = items.reduce((curNumberAcc, item) => {
     return curNumberAcc + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsBumped ? classes.bump: ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    };
+    setBtnIsBumped(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsBumped(false)
+    }, 300);
+
+    // cleanup func
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]); // Cart button is bumped when items in cart change
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
