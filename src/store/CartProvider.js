607d11f7199a9ceb9ae19d6811a8327934971afa
xzemanov <zemanovicova.dominika@gmail.com>
2022-08-26 23:28:09 +0200

    adding items in cart works

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -15,8 +15,6 @@ const cartReducer = (state, action) => {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
-  
-    // null if new item
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
@@ -28,13 +26,32 @@ const cartReducer = (state, action) => {
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
     } else {
-      // NEW copy of items, important
       updatedItems = state.items.concat(action.item);
     }
 
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
     };
   }
 
