619fe80fdd7330e7da65ca31859f00c759368ed9
xzemanov <zemanovicova.dominika@gmail.com>
2022-08-30 00:36:37 +0200

    order data are sent to database

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,42 +1,60 @@
-import { useContext, useState } from "react";
-import Modal from "../UI/Modal";
-import CartContext from "../../store/cart-context";
-import CartItem from "./CartItem";
-import classes from "./Cart.module.css";
-import Checkout from "./Checkout";
+import { useContext, useState } from 'react';
+
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
+
+const firebaseAddress = "https://my-dummy-firebase-link/orders.json"
 
 const Cart = (props) => {
-  const cartCtx = useContext(CartContext);
   const [isCheckout, setIsCheckout] = useState(false);
+  const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemAddHandler = (item) => cartCtx.addItem({ ...item, amount: 1 });
-  const cartItemRemoveHandler = (id) => cartCtx.removeItem(id);
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem(item);
+  };
 
   const orderHandler = () => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch(firebaseAddress, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+      })
+    });
+  };
+
   const cartItems = (
-    <ul className={classes["cart-items"]}>
+    <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onAdd={cartItemAddHandler.bind(null, item)}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
-        ></CartItem>
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
 
   const modalActions = (
     <div className={classes.actions}>
-      <button className={classes["button--alt"]} onClick={props.onClose}>
+      <button className={classes['button--alt']} onClick={props.onClose}>
         Close
       </button>
       {hasItems && (
@@ -54,7 +72,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
